8be45a49ada8fb40154ae0d5e072281df8e4a785
manishapariyar <manishapariyar196@gmail.com>
2024-07-25 13:50:44 +0545

    Added dependencies, updated package.json and package-lock.json, modified App.jsx, index.css, and main.jsx files

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar.jsx'
+import Sidebar from './components/siderbar/Sidebar.jsx'
+
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <hr/>
+      <div className="app-content">
+        <Sidebar/>
+      </div>
     </div>
   )
 }
