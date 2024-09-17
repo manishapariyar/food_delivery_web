85f1728c2fb8a9485cebb866cd86a3b700a18046
manishapariyar <manishapariyar196@gmail.com>
2024-09-18 02:16:48 +0545

    "Updated Frontend and Backend code: modified CSS files, removed ESEWA secrets from .env, and commented out orderRouter import in server.js"

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import "dotenv/config.js";
 import cartRouter from "./routes/cartRoute.js";
-import orderRouter from "./routes/orderRoute.js";
+// import orderRouter from "./routes/orderRoute.js";
 //app config
 const app = express();
 const port = 3000;
